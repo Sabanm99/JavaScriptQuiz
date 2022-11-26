a0ce32341acd04ca16cea0f09e20ab37c931c7a8
Sabanm Akter <108559316+Sabanm99@users.noreply.github.com>
2022-11-26 16:59:54 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,113 @@
+//QUESTION DATA
+const QUESTIONS = [
+    {
+        question:'How many bones are in the adult human body?',
+        answers: [ 
+            '500', 
+            '206', 
+            '107',
+            '300',
+            ],
+        correctAnswer: '206',
+        explanation: 'There are 206 bones in the adult human body. A newborn baby will have 300, but many fuse during development.'
+    },
+    {
+        question:'What is the primary function of muscles?',
+        answers: [
+            'Movement', 
+            'Heat Production', 
+            'Stability', 
+            'All of the above' 
+            ],
+        correctAnswer: 'All of the above',
+        explanation: 'The muscular system carries out all of these functions. Each one is vital to survival.'
+    },
+    {
+        question:'What part of the brain is responsible for providing feedback on the position of the body in order to coordinate movement?',
+        answers: [
+            'Cerebrum', 
+            'Midbrain', 
+            'Cerebellum', 
+            'The Limbic System' 
+            ],
+        correctAnswer: 'Cerebellum',
+        explanation: 'The Cerebellum compares indended movement patterns with actual movement patterns in order to carry out better future patterns.'
+    },
+    {
+        question:'Which of the following controls body temperature, sleep, and appetite?',
+        answers: [
+            'Pancreas', 
+            'Thalamus', 
+            'Thyroid gland', 
+            'Hypothalamus' 
+            ],
+        correctAnswer: 'Hypothalamus',
+        explanation: 'The hypothalamus helps manage your body temperature, hunger and thirst, mood, sex drive, blood pressure and sleep.'
+    },
+    {
+        question:'Which muscle/s is/are responsible for closing the jaw?',
+        answers: [
+            'Masseter and Temporalis', 
+            'Pterygoids', 
+            'Trapezius and Sternocleidomastoid', 
+            'Deltiods' 
+            ],
+        correctAnswer: 'Masseter and Temporalis',
+        explanation: 'The masseter elevates the jaw, closing the mouth.The temporalis elevates and retracts the jaw.'
+    },
+    {
+        question:'Which direction do arteries carry blood?',
+        answers: [
+            'Toward the heart', 
+            'Away from the bones', 
+            'Away from the heart', 
+            'To the brain only' 
+            ],
+        correctAnswer: 'Away from the heart',
+        explanation: 'Arteries are responsible for carrying blood away from the heart while veins are responsible for carrying blood to the heart.'
+    },
+    {
+        question:'Which of the following is the thinnest blood vessel?',
+        answers: [
+            'Veins', 
+            'Arterioles', 
+            'Meta Arterioles', 
+            'Capillaries' 
+            ],
+        correctAnswer: 'Capillaries',
+        explanation: 'Capillaries contain a single cell barrier and are the site of nutrient and gas exchange between the blood and the body.'
+    },
+    {
+        question:'Which type of joint lies at the shoulder joint and hip joint',
+        answers: [
+            'Pivot joint', 
+            'Ball and Socket joint', 
+            'Rotation joint', 
+            'Hinge joint' 
+            ],
+        correctAnswer: 'Ball and Socket joint',
+        explanation: 'Ball and socket joints have a round head. They are able to move freely in any direction.'
+    },
+    {
+        question:'An electrocardiogram is a graphic illustration of?',
+        answers: [
+            'Cardiac conduction system', 
+            'Respiratory cycle', 
+            'Cardiac output', 
+            'Cardiac cycle' 
+            ],
+        correctAnswer: 'Cardiac conduction system',
+        explanation: 'Electrocardiograms measure the electrical activity of the heart.'
+    },
+    {
+        question:'The brain and spinal cord together make up?',
+        answers: [
+            'Central nervous system', 
+            'Main nervous system', 
+            'Peripheral nervous sustem', 
+            'Nerons' 
+            ],
+        correctAnswer: 'Central nervous system',
+        explanation: 'The central nervous system consists of the brain and spinal chord.'
+    }
+];
